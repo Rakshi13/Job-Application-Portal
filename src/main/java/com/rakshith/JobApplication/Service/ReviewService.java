237105e0aaa158d65b954625d6ccc73c56780be5
Rakshi13 <rakshithg683@gmail.com>
2026-05-25 23:22:35 +0530

    Added Relationship between the Review and Company. One to Many Relationship = COMPANY-REVIEW Many to One Relationship = REVIEW-COMPANY
    Also updated the code and performed CRUD Operations.

---
+++ src/main/java/com/rakshith/JobApplication/Service/ReviewService.java
@@ -0,0 +1,23 @@
+package com.rakshith.JobApplication.Service;
+
+import com.rakshith.JobApplication.Entity.Review;
+
+import java.util.List;
+
+public interface ReviewService {
+
+    //ADD REVIEW
+    Boolean addCompanyReview(Review review,Long companyId);
+
+    //GET ALL REVIEWS
+    List<Review> getAllCompanyReview(Long companyId);
+
+    //UPDATE REVIEW
+    Boolean updateCompanyReview(Review review,Long id);
+
+    //GET COMPANY SPECIFIC REVIEW
+    Review getCompanySpecificReview(Long id);
+
+    //DELETE REVIEW BY ID
+    Boolean deleteReviewById(Long id);
+}
