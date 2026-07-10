281f334f8137c4a1b95480d2d95a2b20dd6fd6db
Rakshi13 <rakshithg683@gmail.com>
2026-07-10 23:03:32 +0530

    Small changes on registration.

--- Frontend/js/candidate-register.js
+++ Frontend/js/candidate-register.js
@@ -1,43 +1,44 @@
 console.log("Candidate JS Loaded");
 
 document.getElementById("register-candidate")
-.addEventListener("click", async function (e) {
+    .addEventListener("click", async function (e) {
 
-    e.preventDefault();
+        e.preventDefault();
 
-    const request = {
-        username: document.getElementById("candidate-username").value,
-        password: document.getElementById("candidate-password").value,
-        firstName: document.getElementById("candidate-firstname").value,
-        lastName: document.getElementById("candidate-lastname").value,
-        email: document.getElementById("candidate-email").value,
-        mobile: document.getElementById("candidate-mobile").value
-    };
+        const request = {
+            username: document.getElementById("candidate-username").value,
+            password: document.getElementById("candidate-password").value,
+            firstName: document.getElementById("candidate-firstname").value,
+            lastName: document.getElementById("candidate-lastname").value,
+            email: document.getElementById("candidate-email").value,
+            mobile: document.getElementById("candidate-mobile").value
+        };
 
-    const response = await fetch("http://localhost:8080/candidate/register", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(request)
+        const response = await fetch("http://localhost:8080/candidate/register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(request)
+        });
+
+        const data = await response.json();
+
+        console.log(data);
+
+        if (response.ok) {
+
+            alert(data.message);
+            debugger
+            setTimeout(() => {
+                debugger
+                window.location.href = "login.html";
+            }, 1000);
+
+        } else {
+
+            alert(data.message);
+
+        }
+
     });
-    
-    const data = await response.json();
-    
-    console.log(data);
-    
-    if (response.ok) {
-    
-        alert(data.message);
-    
-        setTimeout(() => {
-            window.location.href = "login.html";
-        }, 1000);
-    
-    } else {
-    
-        alert(data.message);
-    
-    }
-
-});
