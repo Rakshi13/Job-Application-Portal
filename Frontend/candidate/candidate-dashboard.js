75b7fd40c0fe08cd1f65252fce0a77a461142015
Rakshi13 <rakshithg683@gmail.com>
2026-08-17 08:29:55 +0530

    Implemented Logout Fucntionality.
    Once after clicking logout button in Employer and Candidate Dashboard . user is able to navigate to Login page.

--- Frontend/candidate/candidate-dashboard.js
+++ Frontend/candidate/candidate-dashboard.js
@@ -1,18 +1,19 @@
 const username = localStorage.getItem("username");
 
 if (!username) {
-    window.location.href = "login.html";
+    window.location.href = "../login.html";
+    return;
 }
 
 document.getElementById("welcomeUser").textContent =
     "Welcome, " + username;
 
-document.getElementById("logoutBtn")
+document.getElementById("logout-btn")
     .addEventListener("click", function () {
 
         localStorage.removeItem("token");
         localStorage.removeItem("username");
         localStorage.removeItem("role");
 
-        window.location.href = "login.html";
+        window.location.href = "../login.html";
     });
